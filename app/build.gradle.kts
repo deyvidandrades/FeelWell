16d9bd68c7333f71828b37b2dedd03ad8cfc4fd4
deyvidandrades <deyvid-asilva@hotmail.com>
2025-09-02 17:09:39 -0300

    * Terms link update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         minSdk = 35
         targetSdk = 36
         versionCode = 5
-        versionName = "0.1.10"
+        versionName = "0.1.11"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
